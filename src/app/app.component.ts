e6923dbfac892440aeb71e416d5762be7edbf5c1
Sahil Singh Chauhan <sahilchauhan5467@gmail.com>
2025-03-07 12:36:04 +0530

    Sahil, Added :- Error msg representation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -14,5 +16,15 @@ export class AppComponent {
   openBridgeLabzSite(): void {
     window.open('https://www.bridgelabz.com', '_blank');
   }
-
+  userName: string = '';
+  errorMessage: string = '';
+  validateUserName() {
+    const namePattern = /^[A-Z]{1}[a-zA-Z]{2,}$/; // Starts with uppercase, min 3 letters
+    if (!namePattern.test(this.userName)) {
+      this.errorMessage =
+        'Invalid name! Start with a capital letter and use at least 3 characters.';
+    } else {
+      this.errorMessage = '';
+    }
+  }
 }
