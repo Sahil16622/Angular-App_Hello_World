2115ccdf56d31b2fea3f392cf0c36bfc582d8d99
Sahil Singh Chauhan <sahilchauhan5467@gmail.com>
2025-03-07 12:28:45 +0530

    Sahil, Added :- Connecting img and link to visit the bridgelabz site o clicking the link.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Hello-world-app';
+  title = 'HelloWorld';
   message: string = 'Hello from BridgeLabz using One Way Data Bindings';
   logoUrl: string = 'BL_logo_square_jpg.jpg';
-  openBridgeLabzSite(): void {
+  openBridgeLabzSite(){
     window.open('https://www.bridgelabz.com', '_blank');
   }
-
+  userName: string = '';
 }
